3fe515c2f209447ba8ef8a5f3455d4e75dcf6040
its-Aditya-777 <kv022588@gmail.com>
2024-11-20 17:35:51 +0530

    Final Commit

--- app.py
+++ app.py
@@ -45,7 +45,8 @@ def search_student():
 
 #changing port from (5000 to 8000).
 if __name__== "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
 #create two directories static and template.
 
 #final-> Deployment of app(webpage) on Heroku.
